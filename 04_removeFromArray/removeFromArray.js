46ab4faef22b909a609dcda765b9e7aa8137ae66
Josh Mathiak <jmathiak4@gmail.com>
2022-02-12 14:21:57 -0600

    solved q4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+  let args = Array.from(arguments);
+  let workingArray = args[0];
+  for (let i = 1; i < args.length; i++) {
+    let ind = workingArray.indexOf(args[i]);
+    if (ind !== -1) {
+      workingArray.splice(ind, 1);
+    }
+  }
+  return workingArray;
 };
 
 // Do not edit below this line

7f5e984aaca456f7456eeda6547c5aa142b39b58
Josh Mathiak <jmathiak4@gmail.com>
2022-02-12 14:51:23 -0600

    solved q7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let ans = (temp - 32) * (5 / 9);
+  let ret = Math.round(ans * 10) / 10;
+  return ret;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let ans = temp * (9 / 5) + 32;
+  let ret = Math.round(ans * 10) / 10;
+  return ret;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };

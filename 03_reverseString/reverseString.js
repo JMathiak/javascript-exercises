c65afa9b7e6305f2e145a03fa05e5d3391cf39b0
Josh Mathiak <jmathiak4@gmail.com>
2022-02-12 14:03:44 -0600

    completed q3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let workingArray = string.split("");
+  let ans = [];
+  for (let i = workingArray.length - 1; i >= 0; i--) {
+    ans.push(workingArray[i]);
+  }
+  let ret = ans.join("");
+  return ret;
 };
 
 // Do not edit below this line

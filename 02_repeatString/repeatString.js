1aa939b5b939f4a7d82e3b8df6d46817903d3462
Josh Mathiak <jmathiak4@gmail.com>
2022-02-12 13:56:54 -0600

    Completed assignments 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let ans = "";
+  if (num === 0) {
+    return ans;
+  }
+  if (num === 1) {
+    return string;
+  }
+  if (num < 0) {
+    return "ERROR";
+  }
+  if (string === "") {
+    return string;
+  }
+  for (let i = 0; i < num; i++) {
+    ans += string;
+  }
+  return ans;
 };
 
 // Do not edit below this line

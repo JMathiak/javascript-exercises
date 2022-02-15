9d6bcbdf256608690fca29f0cfe12220a6564fae
Josh Mathiak <jmathiak4@gmail.com>
2022-02-15 17:54:55 -0600

    completed q8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (n1, n2) {
+  let prod = n1;
+  for (i = 1; i < n2; i++) {
+    prod *= n1;
+  }
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let sum = 1;
+  if (num === 0) {
+    return 1;
+  } else {
+    for (i = 1; i <= num; i++) {
+      sum *= i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
